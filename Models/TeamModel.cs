7017af730af46bce8a74adee2c5dc35d78b05eb3
Kody Kriner <kody@pingman.com>
2018-12-12 19:01:57 -0700

    Adding TeamModel

---
+++ Models/TeamModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ff_platform.Models
+{
+    public class TeamModel
+    {
+        public int ID { get; set; }
+        public string OwnerID { get; set; }
+        public string LeagueID { get; set; }
+        public List<string> PlayerIDs { get; set; }
+    }
+}
