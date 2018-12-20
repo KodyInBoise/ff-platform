8eb2dcc076dc59c0203660bc6709dd96cd722ca7
Kody Kriner <kody@pingman.com>
2018-12-19 21:23:40 -0700

    Added NFLSeason Model

---
+++ Models/NFLSeasonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ff_platform.Models
+{
+    public class NFLSeason
+    {
+        public Guid ID { get; set; }
+        public int Year { get; set; }
+        public int PreseasonWeekCount { get; set; }
+        public int RegularSeasonWeekCount { get; set; }
+        public int PostSeasonWeekCount { get; set; }
+        public int TotalWeekCount { get; set; }
+        public DateTime PreaseasonStart { get; set; }
+        public DateTime RegularSeasonStart { get; set; }
+        public DateTime PostSeasonStart { get; set; }
+    }
+}
