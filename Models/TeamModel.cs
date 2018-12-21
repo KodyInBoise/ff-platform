7178f2ea6fd4178975763e00f9887afa634c4d03
Kody Kriner <kody@pingman.com>
2018-12-20 21:31:57 -0700

    Some work on cache util and updated TeamModel properties

--- Models/TeamModel.cs
+++ Models/TeamModel.cs
@@ -5,8 +5,8 @@ namespace ff_platform.Models
 {
     public class TeamModel
     {
-        public int ID { get; set; }
-        public string OwnerID { get; set; }
+        public Guid ID { get; set; }
+        public Guid OwnerID { get; set; }
         public Guid LeagueID { get; set; }
         public List<string> PlayerIDs { get; set; }
     }
