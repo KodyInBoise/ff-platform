b0b150d8a39c4f7004d5bd8bab7ca94291ab9810
Kody-PC\Kody <kody.kriner@gmail.com>
2018-12-20 20:54:32 -0700

    Added UserProfileName migration

---
+++ Migrations/20181221035406_UserProfileName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ff_platform.Migrations
+{
+    public partial class UserProfileName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "UserProfiles",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "UserProfiles");
+        }
+    }
+}
