21c6879b8b68b96e7500674bca34d918f8b42517
Kody-PC\Kody <kody.kriner@gmail.com>
2018-12-20 21:54:50 -0700

    Added TeamModelName migraiton

--- Migrations/20181221045352_TeamModelName.Designer.cs
+++ Migrations/20181221045352_TeamModelName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ff_platform.Data;
@@ -10,9 +11,10 @@ using ff_platform.Data;
 namespace ff_platform.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181221045352_TeamModelName")]
+    partial class TeamModelName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
