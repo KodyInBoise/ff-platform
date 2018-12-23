83ed4cc727db8c1ceac36729a037d36855d4bc6d
KodyMacbook2017 <kody.kriner@gmail.com>
2018-12-22 19:10:25 -0700

    Implementing NFL.com API v2

---
+++ NFL_API/V2/APIHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ff_platform.NFL_API.V2
+{
+    public class APIHelper
+    {
+        static APIHelper _instance { get; set; }
+
+
+        public static void Startup()
+        {
+            _instance = new APIHelper();
+        }
+
+        public static class Endpoints
+        {
+            static string _baseUrl = "";
+        }
+    }
+}
